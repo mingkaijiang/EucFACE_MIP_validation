d42baea521e5f34239fde4bddb599b46143bcb5e
Mingkai Jiang <shanyujordan512@gmail.com>
2021-10-19 21:50:13 +1100

    comparison to Medlyn finished

--- scripts/multi_model_checks/Medlyn_2016/compare_two_MIP_results.R
+++ scripts/multi_model_checks/Medlyn_2016/compare_two_MIP_results.R
@@ -1,7 +1,7 @@
 compare_two_MIP_results <- function() {
     
     ### setting out path to store the files
-    out.dir <- paste0(getwd(), "/output/MIP_output/processed_simulation/Medyn2016")
+    out.dir <- paste0(getwd(), "/output/MIP_output/OBS_output/FIX")
     
     ### create output folder
     if(!dir.exists(out.dir)) {
@@ -18,6 +18,112 @@ compare_two_MIP_results <- function() {
     ### compare the two
     myDF <- merge_this_MIP_and_Medlyn_2016(medDF=medDF, thisDF=thisDF)
     
+    
+    ### compare the two with plotting
+    plotDF1 <- myDF[myDF$variable=="GPP",]
+    
+    p1 <- ggplot(data=plotDF1, 
+                 aes(ModVersion, meanvalue, group=Trt)) +
+        geom_bar(stat = "identity", aes(fill=Trt), 
+                 position=position_dodge(), col="black") +
+        geom_errorbar(aes(x=ModVersion, ymin=meanvalue-sdvalue,
+                          ymax=meanvalue+sdvalue), 
+                      col="black", 
+                      position=position_dodge(width=0.9), width=0.2)+
+        xlab("")+
+        theme_linedraw() +
+        facet_wrap(~MIP)+
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=12),
+              axis.title.x=element_text(size=14),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'horizontal',
+              legend.box.just = 'left',
+              legend.background = element_rect(fill="grey",
+                                               size=0.5, linetype="solid", 
+                                               colour ="black"),
+              plot.title = element_text(size=14, face="bold.italic", 
+                                        hjust = 0.5))+
+        ylab(expression(paste("GPP (g C " * m^2 * " " * yr^-1 * ")")))+
+        scale_fill_manual(name="Trt",
+                          values=c("amb"="blue3", "ele"="red3"))
+    
+    
+    
+    plotDF2 <- myDF[myDF$variable=="NPP",]
+    
+    p2 <- ggplot(data=plotDF2, 
+                 aes(ModVersion, meanvalue, group=Trt)) +
+        geom_bar(stat = "identity", aes(fill=Trt), 
+                 position=position_dodge(), col="black") +
+        geom_errorbar(aes(x=ModVersion, ymin=meanvalue-sdvalue,
+                          ymax=meanvalue+sdvalue), 
+                      col="black", 
+                      position=position_dodge(width=0.9), width=0.2)+
+        xlab("")+
+        theme_linedraw() +
+        facet_wrap(~MIP)+
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=12),
+              axis.title.x=element_text(size=14),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'horizontal',
+              legend.box.just = 'left',
+              legend.background = element_rect(fill="grey",
+                                               size=0.5, linetype="solid", 
+                                               colour ="black"),
+              plot.title = element_text(size=14, face="bold.italic", 
+                                        hjust = 0.5))+
+        ylab(expression(paste("NPP (g C " * m^2 * " " * yr^-1 * ")")))+
+        scale_fill_manual(name="Trt",
+                          values=c("amb"="blue3", "ele"="red3"))
+    
+    
+    plotDF3 <- myDF[myDF$variable=="NEP",]
+    
+    p3 <- ggplot(data=plotDF3, 
+                 aes(ModVersion, meanvalue, group=Trt)) +
+        geom_bar(stat = "identity", aes(fill=Trt), 
+                 position=position_dodge(), col="black") +
+        geom_errorbar(aes(x=ModVersion, ymin=meanvalue-sdvalue,
+                          ymax=meanvalue+sdvalue), 
+                      col="black", 
+                      position=position_dodge(width=0.9), width=0.2)+
+        xlab("")+
+        theme_linedraw() +
+        facet_wrap(~MIP)+
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=12),
+              axis.title.x=element_text(size=14),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position=c(.85,.75),
+              legend.box = 'horizontal',
+              legend.box.just = 'left')+
+        ylab(expression(paste("NEP (g C " * m^2 * " " * yr^-1 * ")")))+
+        scale_fill_manual(name="",
+                          values=c("amb"="blue3", "ele"="red3"))
+    
+    
+    ### Plotting
+    pdf(paste0(out.dir, "/Comparison_to_Medlyn_2016.pdf"), width=4, height=8)
+    
+    grid.arrange(p1, p2, p3, nrow=3)
+    
+    dev.off()
     
     
     
--- scripts/multi_model_checks/Medlyn_2016/merge_this_MIP_and_Medlyn_2016.R
+++ scripts/multi_model_checks/Medlyn_2016/merge_this_MIP_and_Medlyn_2016.R
@@ -2,13 +2,13 @@ merge_this_MIP_and_Medlyn_2016 <- function(medDF, thisDF) {
     
     ### variables to select
     medDF <- medDF[,c("ModVersion", "Trt", "CO2.mean", "PREC.mean",
-                      "CO2.sd", "PREC.sd",
+                      #"CO2.sd", "PREC.sd",
                       "NEP.mean", "GPP.mean", "NPP.mean", 
                       "NEP.sd", "GPP.sd", "NPP.sd")]
     
     ### variables to select
     thisDF <- thisDF[,c("ModVersion", "Trt", "CO2.mean", "PREC.mean",
-                        "CO2.sd", "PREC.sd",
+                        #"CO2.sd", "PREC.sd",
                       "NEP.mean", "GPP.mean", "NPP.mean", 
                       "NEP.sd", "GPP.sd", "NPP.sd")]
     
@@ -20,14 +20,25 @@ merge_this_MIP_and_Medlyn_2016 <- function(medDF, thisDF) {
     myDF <- rbind(medDF, thisDF)
     
     ### split
-    myDF1 <- myDF[,c("ModVersion", "Trt", "CO2.mean", "PREC.mean",
+    myDF1 <- myDF[,c("MIP", "ModVersion", "Trt", "CO2.mean", "PREC.mean",
                      "NEP.mean", "GPP.mean", "NPP.mean")]
     
-    myDF2 <- myDF[,c("ModVersion", "Trt", "CO2.sd", "PREC.sd",
+    myDF2 <- myDF[,c("MIP", "ModVersion", "Trt", 
                        "NEP.sd", "GPP.sd", "NPP.sd")]
     
     ### convert from wide to long
+    outDF1 <- reshape::melt(myDF1, id.vars=c("MIP", "ModVersion", "Trt", 
+                                             "CO2.mean", "PREC.mean"))
+    outDF2 <- reshape::melt(myDF2, id.vars=c("MIP", "ModVersion", "Trt"))
     
+    ### revise names
+    names(outDF1)[names(outDF1)=="value"] <- "meanvalue"
+    names(outDF2)[names(outDF2)=="value"] <- "sdvalue"
     
+    outDF1$variable <- gsub(".mean", "", outDF1$variable)
+    outDF2$variable <- gsub(".sd", "", outDF2$variable)
     
+    outDF <- merge(outDF1, outDF2, by=c("MIP", "ModVersion", "Trt", "variable"))
+    
+    return(outDF)
 }
